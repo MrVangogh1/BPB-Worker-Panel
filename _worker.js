7dfed2df8d078999ae5e566a43571d701e5f25a8
MrVangogh1 <111977669+MrVangogh1@users.noreply.github.com>
2024-07-17 11:42:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '94e465da-34c7-4f94-bdcb-da233797e4fa';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
